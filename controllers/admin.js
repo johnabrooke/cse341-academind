314c92f56bfc4b2a54a92461968b35e5a70be948
John Brooke <bro16077@byui.edu>
2021-10-10 01:37:02 -0700

    add crud delete functionality to delete products

--- controllers/admin.js
+++ controllers/admin.js
@@ -83,8 +83,12 @@ exports.getProducts = (req, res, next) => {
         .catch(err => console.log(err));
 };
 
-// exports.postDeleteProduct = (req, res, next) => {
-//     const prodId = req.body.productId;
-//     Product.deleteById(prodId);
-//     res.redirect('/admin/products');
-// };
+exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.deleteById(prodId)
+        .then(() => {
+            console.log('Destroyed Product');
+            res.redirect('/admin/products');
+        })
+        .catch(err => console.log(err));
+};
