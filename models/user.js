31d538542c90cd24aaa8721461bdb9c7cbe42cdf
John Brooke <bro16077@byui.edu>
2021-10-11 20:21:02 -0700

    add user model

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+
+const ObjectId = mongodb.ObjectId;
+
+class User {
+    constructor(username, email) {
+        this.name = username;
+        this.email = email;
+    }
+
+    save() {
+        const db = getDb();
+        let dbOp;
+        if (this._id) {
+            dbOp = db
+                .collection('users')
+                .udateOne({ _id: this.id }, { $set: this });
+        } else {
+            dbOp = db.collection('users').insertOne(this);
+        }
+        return dbOp
+            .then(result => {
+                console.log(result);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    static findById(userId) {
+        const db = getDb();
+        return db
+            .collection('users')
+            .findOne({ _id: new ObjectId(userId) })
+            .then(user => {
+                console.log(user);
+                return user;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+}
+
+module.exports = User;
