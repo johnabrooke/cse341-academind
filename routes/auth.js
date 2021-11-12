20922d540914cd3d865a3904efafe52708667646
John Brooke <bro16077@byui.edu>
2021-11-12 01:02:07 -0700

    finish validation

--- routes/auth.js
+++ routes/auth.js
@@ -10,15 +10,19 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', 
-[
-    body('email')
-        .isEmail()
-        .withMessage('Please enter a valid email address'),
-    body('password', 'Please enter a valid password, with at least 8 characters.')
-        .isLength({ min: 8 })
-],
-    authController.postLogin);
+router.post(
+    '/login', 
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Please enter a valid email address')
+            .normalizeEmail(),
+        body('password', 'Please enter a valid password, with at least 8 characters.')
+            .isLength({ min: 8 })
+            .trim()
+    ],
+    authController.postLogin
+);
 
 router.post(
     '/signup', 
@@ -34,28 +38,30 @@ router.post(
                 return User.findOne({ email: value })
                     .then(userDoc => {
                         if (userDoc) {
-                            return Promise.reject('An account with that email already exists.  Please login.'
-                            )
-                            .catch(err => {
-                                console.log(err);
-                                res.redirect('/login');
-                            });
+                            return Promise.reject(
+                                'An account with that email already exists.  Please login.'
+                            );
                         }
                     });
-            }),
+            })
+            .normalizeEmail(),
         body(
             'password',
             'Please enter a password with at least 8 characters.'
         )
-            .isLength({ min: 8 }),
-        body('confirmPassword').custom((value, { req }) => {
+            .isLength({ min: 8 })
+            .trim(),
+        body('confirmPassword')
+            .trim()
+            .custom((value, { req }) => {
             if (value !== req.body.password) {
                 throw new Error('Passwords must match.');
             }
             return true;
         })
     ],
-    authController.postSignup);
+    authController.postSignup
+    );
 
 router.post('/logout', authController.postLogout);
 
@@ -63,7 +69,7 @@ router.get('/passwordReset', authController.getPasswordReset);
 
 router.post('/passwordReset', authController.postReset);
 
-router.get('passwordReset/:token', authController.getNewPassword);
+router.get('/passwordReset/:token', authController.getNewPassword);
 
 router.post('/newPassword', authController.postNewPassword);
 
